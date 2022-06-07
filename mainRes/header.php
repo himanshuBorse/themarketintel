5fde110ffae0f41d4a003513f7e1c6d188c37a95
Himanshu Borse <76398669+himanshuBorse@users.noreply.github.com>
2022-06-07 11:40:11 +0530

    Add files via upload

---
+++ mainRes/header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>The Market Intel</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+
+<body class="is-preload">
+
+    <!-- Wrapper -->
+    <div id="wrapper">
+
+        <!-- Header -->
+        <header id="header">
+            <h1><a href="index.php">The Market Intel</a></h1>
+            <nav class="links">
+                <ul>
+                    <li><a href="#">Consumer Goods & Retail</a></li>
+                    <li><a href="#">Tech & Electronics</a></li>
+                    <li><a href="#">IT & Telecom</a></li>
+                    <li><a href="#">Automotive</a></li>
+                    <li><a href="#">Health Care</a></li>
+                    <li><a href="#">Market Research</a></li>
+                </ul>
+            </nav>
+            <nav class="main">
+                <ul>
+                    <li class="search">
+                        <a class="fa-search" href="#search">Search</a>
+                        <form id="search" method="get" action="#">
+                            <input type="text" name="query" placeholder="Search" />
+                        </form>
+                    </li>
+                    <li class="menu">
+                        <a class="fa-bars" href="#menu">Menu</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- Menu -->
+        <section id="menu">
+
+            <!-- Search -->
+            <section>
+                <form class="search" method="get" action="#">
+                    <input type="text" name="query" placeholder="Search" />
+                </form>
+            </section>
+
+            <!-- Links -->
+            <section>
+                <ul class="links">
+                    <li>
+                        <a href="#">
+                            <h3>Home</h3>
+
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <h3>About Us</h3>
+
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <h3>Contact Us</h3>
+
+                        </a>
+                    </li>
+                    <!-- <li>
+                        <a href="#">
+                            <h3>Etiam sed consequat</h3>
+                            <p>Porta lectus amet ultricies</p>
+                        </a>
+                    </li> -->
+                </ul>
+            </section>
+
+            <!-- Actions -->
+            <section>
+                <ul class="actions stacked">
+                    <li><a href="#" class="button large fit">Log In</a></li>
+                </ul>
+            </section>
+
+        </section>
---
+++ single.php
@@ -0,0 +1,116 @@
+<!-- Start Header  -->
+<?php include("./blogRes/header.php"); ?>
+<!-- End Header -->
+
+<!-- Main -->
+<div id="main">
+
+	<!-- Post -->
+	<article class="post">
+		<header>
+			<div class="title">
+				<h2><a href="#">Semiconductor chip shortage till 2023 or overproduction by 2025?</a></h2>
+				<p>
+					Risk sharing to play key role during disruption wherein businesses can collaborate to share digital & infra resources
+				</p>
+			</div>
+			<div class="meta">
+				<time class="published" datetime="2015-11-01">April 22, 2022</time>
+				<a href="#" class="author"><span class="name">Yogesh Desale</span><img src="images/avatar.jpg" alt="" /></a>
+			</div>
+		</header>
+		<span class="image featured"><img src="images/pic01.jpg" alt="" /></span>
+		<span>
+			<p>
+				Over the last 2 years, semiconductor chip shortage has been one of the critical issues in the tech, electronics, and automotive industries. The unavailability of chip set and electronics components have materially impacted businesses in terms of revenue and customer experience.
+			</p>
+			<p>
+				The shortage of semiconductor chips started early in the pandemic wherein the government enforced lock downs across nations. As a result, factories have been shut down for months resulting in production halting and businesses stopped ordering new chips and components being part of the Just-in-time (JIT) supply chain. This strategy has failed as demand for specific consumer electronics and appliances such as laptops, mobile phones/smartphones, personal care devices, and home appliances have increased greatly across the globe. This demand is attributed to the change in consumer behavior, remote work and education culture influenced by the pandemic. 
+			</p>
+			<p>
+			The global industry has seen a bullwhip effect due to supply chain crisis on the supply side leading to higher lead time for delivery of chips and unprecedented demand at the end-consumers. In addition to this, the automotive sector also demands a sizable number of chip sets and lacking single components can hamper the production cycle of automotive vehicles. Also, automotive ecosystem is highly dependent on Tier1, Tier2, and Tier3 suppliers, which are prone to supply chain disruption to a larger extent. <a href="https://www.scmp.com/tech/tech-war/article/3128060/tsmc-spend-us100-billion-over-three-years-grow-chip-capacity">  Automotive industry has estimated to loss revenue of USD (US Dollar) 60 billion in 2021 due to chip shortage.</a>
+			</p>
+			<p>
+			To cope with higher customer demand and supply chain crisis, businesses in tech, electronics, and automotive have started developing short- and long-term strategies to streamline production of chips and shorten lead time. Businesses adopted key strategies, including visibility into supply chain, inventory management, predicting supply and demand with the help of analytic and AI tools and strengthening physical supply chain by improving transportation and logistics capabilities in a short term. 
+			</p>
+			<p>
+			On a top of that, as a part of long-term strategy, businesses are also localizing and diversifying supply chain with the help of signing Free Trade Agreements (FTAs), near shoring, opening new production facilities in the North America, Europe, Latin America, and Southeast Asia considering current production of semiconductor chip sets manufacturers are in Northeast Asia, China, Japan, South Korea, and Taiwan, which is hub of fabless semiconductor companies namely MediaTek, TSMC (Taiwan Semiconductor Manufacturing Company), and UMC. 
+			</p>
+			<p>
+			Government authorities also acknowledge the shortage of semiconductor chips affecting businesses and economies at a higher level. Hence, several initiatives have been undertaken by US (United States), UK (United Kingdom), European Union, China, Japan, India, and South Korea to support businesses with the help of amendment in the existing regulations, policies, and heavy capital funding to develop new production facilities across region. 
+			</p>
+			<h3>
+			Following are the key Initiatives undertaken by businesses and government authorities
+			</h3>
+			<p>
+			<a href="https://ec.europa.eu/commission/presscorner/detail/en/ip_22_729">Europe Chip Act</a>: The European Chips Act will help businesses strengthen R&D (Research & Development), innovation, and development in the semiconductor industry. Approximately 43 billion Euros fund announced with the inclusion of public and private investment to support this initiative. It will help the EU (European Union) to achieve its goal of reaching 20% of the market share in chips by 2030.
+			</p>
+			<p>
+			<a href="https://apnews.com/article/technology-business-european-union-pat-gelsinger-europe-aa454490c62a2942276acb5f4cbcfc08">Intel unveils USD 88 billion chipmaking expansion plan for Europe</a>: As a part of Europe Chip Act in the first phase, <a href="https://www.intel.com/content/www/us/en/newsroom/news/eu-news-2022-release.html"> Intel has announced investment of 17 billion </a> euros in Germany setting up a semiconductor fab mega factory, R&D and design hub in France, invest in R&D, manufacturing and foundry services in Ireland, Italy, Poland, and Spain. In addition to this, company is planning to spend more 12 billion euros in its Leixlip, Ireland, doubling the manufacturing space to bring Intel 4 process technology to Europe and expand foundry services. Also, Intel and Italy announced agreement to develop back-end manufacturing facility with plan to invest up to 4.5 billion euros. In total, the company plans to invest 33 billion euros in the semiconductor manufacturing sector in the first phase. 
+			</p>
+			<p>
+			<a href="https://www.csis.org/blogs/perspectives-innovation/chips-america-act-why-it-necessary-and-what-it-does#:~:text=Research%20and%20Development%3A%20The%20Act,and%20secure%20microelectronic%20supply%20chains.%E2%80%9D"> CHIPS for America Act</a>: In January 2021, 2021, Congress passed the Creating Helpful Incentives to Produce Semiconductors (CHIPS) for America Act. Under this initiative, In June of 2021, the US. Senate passed the United States Innovation and Competition Act (USICA) to fund USD 39 billion for semiconductor production plans development and other USD 11.2 billion for R&D activities in semiconductor sector.
+			</p>
+			<p>
+			<a href="https://www.scmp.com/tech/tech-war/article/3126719/intel-spend-billions-revive-chip-manufacturing-us-and-chase-tsmc?module=inline&pgtype=article"> Intel to invest USD 20 billion in two new Arizona plants in the US</a>: The company has planned to create a new production unit called Intel Foundry Services which aims to serve to US and Europe market. The company has also announced expansion plans into Europe and other parts of the world. 
+			</p>
+			<p>
+			<a href="https://www.businesswire.com/news/home/20211123005566/en/Canada%E2%80%99s-Semiconductor-Council-Releases-Action-Plan-Aims-to-Build-Chip-Fuelled-Economy-by-2050https:/www.businesswire.com/news/home/20211123005566/en/Canada%E2%80%99s-Semiconductor-Council-Releases-Action-Plan-Aims-to-Build-Chip-Fuelled-Economy-by-2050">Canada’s Semiconductor Action Plan: Roadmap to 2050</a> : Canada’s Semiconductor Council releases a comprehensive action plan to transform domestic semiconductor industry. The action plan includes strengthen and diversify supply chain, develop onshore manufacturing, set up unique specialization in design and R&D and EVs (electric vehicles), batteries and sensors, and foster innovation through investment and government funding. <a href="https://electronics360.globalspec.com/article/17841/canada-to-spend-240-million-to-grow-chip-manufacturing"> The Government of Canada will invest some USD 240 million</a> to bolster the country's expansion into photonics and the manufacturing of semiconductors. 
+			</p>
+			<p>
+			<a href="https://www.business-standard.com/article/international/s-korean-chipmakers-to-invest-47-36-bn-amid-heated-global-competition-122022000189_1.html"> South Korean chipmakers to invest USD 47.36 billion to compete with global leaders</a>: Country’s Semiconductor Industry Association announced investment plan in 2022, including chipmakers such as Samsung Electronics and SK Hynix. <a href="https://www.cnbc.com/2021/05/17/semiconductors-nations-deploy-wartime-like-efforts-to-win-chip-race.html"> South Korea plans to invest 510 trillion South Korean won (USD 452 billion) in chips by 2030</a>
+			</p>
+			<p>
+			<a href="https://www.taipeitimes.com/News/biz/archives/2021/11/27/2003768576">Japan government approved ￥774 billion (USD 6.8 billion) in funding for domestic semiconductor investment</a> : The package includes 3 parts, including ￥617 billion fund into innovative chip manufacturing production capacity, ￥47 billion for legacy production (analog chips and power management parts), and ￥110 billion for the R&D of next-generation silicon. 
+			</p>
+			<p>
+			<a href="https://news.samsung.com/global/samsung-electronics-to-boost-investment-in-logic-chip-businesses-to-krw-171-trillion-by-2030"> Samsung Electronics has planned to invest 171 trillion won in non-memory chips through 2030</a>: This funding will support the company’s foundry business in developing next generation technologies, including AI (Artificial Intelligence), 5G, and autonomous driving. The company has begun construction of a new production line in Pyeongtaek, Korea, which is expected to be completed in the second half of 2022. This production plant will produce 14-nanometer DRAM and 5-nanometer logic semiconductors, both based on extreme ultraviolet (EUV) lithography technology. 
+			</p>
+			<p>
+			<a href="https://www.scmp.com/tech/tech-war/article/3128060/tsmc-spend-us100-billion-over-three-years-grow-chip-capacity"> TSMC’s USD 100 billion investment over 3 years to grow chip production capacity</a>: This investment will help the company to increase production of advanced semiconductor chips with the help of investment in R&D and manufacturing. <a href="https://www.reuters.com/technology/sony-invest-500-mln-tsmcs-new-chip-unit-japan-2021-11-09/"> TSMC is planning to expand chip production capacity with investment of USD 12 billion chip fabrication plant in the US state of Arizona</a>. <a href="https://www.reuters.com/technology/sony-invest-500-mln-tsmcs-new-chip-unit-japan-2021-11-09/"> TSMC and Sony to invest USD 7 billion for new Japanese chip plant </a>: The plant will produce 22-and 28-nanometer chips and supply semiconductors to Sony's image sensor business. 
+			</p>
+			<p>
+			<a href="https://www.scmp.com/tech/policy/article/3085362/china-has-new-us14-trillion-plan-seize-worlds-tech-crown-us"> China’s USD 1.4 trillion investment plan to become leader in tech world</a>: The Chinese government has directed an estimated USD 150 billion to China’s semiconductor industry and has <a href="https://crsreports.congress.gov/product/pdf/R/R46767"> given USD 1.4 trillion for strategic industries, including semiconductors</a>, in its 14th Five-Year Plan (2021-2025). In March 2021, the Shenzhen government announced a USD 2.3 billion investment and a 23% stake in China’s leading foundry, Semiconductor Manufacturing International Corporation. 
+			</p>
+			<p>
+			UMC to build USD 5 billion wafer fab facility in Singapore: This facility will produce 30,000 wafers per month with actual production expected to start by 2024. The production site will be equipped with advanced manufacturing foundries and will produce 22 and 28 nanometer chips. 
+			</p>
+			<p>
+			<a href="https://www.taipeitimes.com/News/biz/archives/2021/12/01/2003768792"> MediaTek to invest US$3.3bn in low-power tech</a>: This funding will mobilize in the R&D related to computing, low power, and advanced chip packaging. The investment is part of the company’s vision to lead the industry over the next 10 years and increase revenue by 15% in the next 3 years. 
+			</p>
+			<p>
+			<a href="https://techcrunch.com/2022/02/22/bosch-invest-296m-semiconductor-production-chip-shortage/"> Bosch to invest USD 296 million in semiconductor production</a>: This funding will go into Reutlingen to create new production space. This facility will produce 6- and 8-inch wafer fabs. The response is to meet growing demand of semiconductor and micro electromechanical systems (MSME) sensors in both automotive and consumer electronics segments. 
+			</p>
+			<p>
+			<a href="https://indianexpress.com/article/business/five-firms-proposals-semiconductor-display-plants-7781496/"> India gets US$20bn in investment bids for chip fabs</a>: India received proposals from following companies, namely Vedanta Foxconn JV (Joint Venture) with Hon Hai Precision Industry Co, IGSS Ventures and ISMC to set up semiconductor chip production plants in India with investment of USD 13.6 billion. In addition to this, India government also plans to invest USD 5.6 billion under Semicon India Program. 
+			</p>
+			<p>
+			<a href="https://www.thehindu.com/business/vedanta-to-invest-up-to-20-bn-in-semiconductor-business-in-india-roll-out-by-2025/article65062876.ece"> Vedanta to invest up to USD 20 billion in semiconductor business in India</a>: The company will invest USD 15 billion into the electronic chip and display manufacturing unit with extended investment to USD 20 billion. The company will launch display units used in mobile phones and electronic devices by 2024 and electronic chips by 2025. 
+			</p>
+			<h3>
+			Impact on the global supply chain
+			</h3>
+			<p>
+			The large-scale developments in the localization and diversification of supply chain would create a new revenue opportunity for all stakeholders, specifically supply chain and logistics players. The sustainable long-term roadmap can help logistics players to support sectors’ agenda to localize and globalize the supply chain by developing a strong and resilient regional and global logistics infrastructure. It is imperative for logistics sector to find favorable near-shoring locations (South America, central and eastern Europe, MEA, and Southeast Asia) and build an infrastructure route with the combination of road and rail (regional and local supply chain) and sea and air (diversified supply chain). Moreover, technology and innovations will help to bridge the gap between supply chain crisis at upstream and midstream and uneven demand at the downstream level with the help of visibility at each stage and sharing critical information across value chain to make informed decision in advance. The risk sharing in the supply chain will play a significant role during disruption wherein businesses can collaborate to share their digital and infrastructure resources to help mitigate larger impact on business revenue. 
+			</p>
+			<h4>Note</h4>
+			<blockquote>Opinions expressed in the article are those of the author, and do not reflect the view of associated organization.</blockquote>
+									
+		</span>
+
+
+		<footer>
+			<ul class="stats">
+				<h4>Author</h4>
+				<blockquote>Yogesh Desale, Research Analyst, 6+ years of experience in market intelligence and strategy, tracking new revenue opportunities in the area of supply chain and technology across verticals. </blockquote>
+				<li><a href="#">General</a></li>
+				<li><a href="#" class="icon solid fa-heart">28</a></li>
+				<li><a href="#" class="icon solid fa-comment">128</a></li>
+			</ul>
+		</footer>
+	</article>
+
+</div>
+<!-- Start Footer  -->
+<?php include("./blogRes/footer.php"); ?>
+<!-- End Footer -->
